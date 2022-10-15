72a13343209aba12579bb2f5e0ab92ed2a836212
arseniov <83009979+arseniov@users.noreply.github.com>
2022-10-15 10:59:49 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/mapbox/light-v10',
+  accessToken:
+    'pk.eyJ1IjoidmlnYW5vZ2lhY29tbyIsImEiOiJjbDk5bzhmMWIwZW44M29vNnlodWwzaWxiIn0.a91rFuN-27ewiLY1IkRB8Q',
+  CSV: './Sample_Data.csv',
+  center: [-120.234, 47.398],
+  zoom: 6,
+  title: 'Test app map',
+  description:
+    'Test lunch ticket map.',
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
+  popupInfo: ['Location_Name'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Languages supported: ',
+      columnHeader: 'Languages',
+      listItems: [
+        'Amharic',
+        'ASL',
+        'Cambodian',
+        'Chinese',
+        'Danish',
+        'English',
+        'French',
+        'German',
+        'Greek',
+        'Hindi',
+        'Italian',
+        'Japanese',
+        'Korean',
+        'Language Line Services',
+        'Norwegian',
+        'Oriya',
+        'Portuguese',
+        'Punjabi',
+        'Russian',
+        'Somali',
+        'Spanish',
+        'Swedish',
+        'Tagalog',
+        'Thai',
+        'Tigrinya',
+        'Tongan',
+        'Vietnamese',
+        'Ukranian',
+      ],
+    },
+    {
+      type: 'checkbox',
+      title: 'Devices available: ',
+      columnHeader: 'Devices_available', // Case sensitive - must match spreadsheet entry
+      listItems: ['Computer', 'Wi-Fi', 'Adaptive Laptops'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+    {
+      type: 'dropdown',
+      title: 'Clients: ',
+      columnHeader: 'Clients',
+      listItems: [
+        'Adults',
+        'Disabled',
+        'Homeless',
+        'Immigrants/Refugees',
+        'Low Income',
+        'Seniors',
+        'Youth: Pre-teen',
+        'Youth: Teen',
+      ],
+    },
+  ],
+};
